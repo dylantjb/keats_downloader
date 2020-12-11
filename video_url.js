726af1d7df14a96ce8a61dc96cc23082b907034f
Dylan Barker <dthomasbarker@gmail.com>
2020-12-11 18:45:55 +0000

    Delete video_url.js

--- video_url.js
+++
@@ -1,9 +0,0 @@
-iframe = window.document.getElementById("contentframe").contentDocument.getElementById("kplayer_ifp").contentDocument
-videoTag = iframe.getElementsByClassName("persistentNativePlayer nativeEmbedPlayerPid")[0]
-
-url = videoTag.src
-sub = null;
-if (videoTag.children.length > 0)
-	sub = videoTag.children[0].src
-
-return [url,sub]
