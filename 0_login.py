89c5f69993839647c4cd2db88de254097391114b
Dylan Barker <dthomasbarker@gmail.com>
2020-12-11 17:11:20 +0000

    Remove redundant import and prompt user for close

--- 0_login.py
+++ 0_login.py
@@ -1,8 +1,9 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
 
 options = webdriver.ChromeOptions()
 options.add_argument("user-data-dir=selenium/chrome_driver")
-driver = webdriver.Chrome(executable_path="selenium/chromedriver.exe", chrome_options=options)
+driver = webdriver.Chrome(executable_path="selenium/chromedriver", options=options)  # for Windows add .exe
 
 driver.get("https://keats.kcl.ac.uk/")
+
+print("Close chrome browser when finished")
