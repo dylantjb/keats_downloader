ccc3c3511f0594cc412670ba603e069223b1cbcf
Dylan Barker <dthomasbarker@gmail.com>
2020-12-11 17:09:15 +0000

    Update database name and follow PEP 8

--- 0_create_db.py
+++ 0_create_db.py
@@ -1,7 +1,9 @@
 import sqlite3
 
-statement = "CREATE TABLE \"Videos\" ( `course` TEXT NOT NULL, `courseID` TEXT NOT NULL, `week` TEXT NOT NULL, `name` TEXT NOT NULL, `pageUrl` TEXT NOT NULL UNIQUE, `videoUrl` TEXT, `srtUrl` TEXT, `file_exists` INTEGER DEFAULT 0, PRIMARY KEY(`pageUrl`) )"
+statement = "CREATE TABLE \"Videos\" ( `course` TEXT NOT NULL, `courseID` TEXT NOT NULL, `week` TEXT NOT NULL, " \
+            "`name` TEXT NOT NULL, `pageUrl` TEXT NOT NULL UNIQUE, `videoUrl` TEXT, `srtUrl` TEXT, `file_exists` " \
+            "INTEGER DEFAULT 0, PRIMARY KEY(`pageUrl`) ) "
 
-database = sqlite3.connect("example.db")
+database = sqlite3.connect("main.db")
 database.execute(statement)
 database.commit()
