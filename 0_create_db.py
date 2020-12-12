cf2aa113408c5835e6dcdc5f5873bc2d141af74c
Dylan Barker <dthomasbarker@gmail.com>
2020-12-12 10:14:26 +0000

    Remove videoUrl and srtUrl columns

--- 0_create_db.py
+++ 0_create_db.py
@@ -1,8 +1,8 @@
 import sqlite3
 
 statement = "CREATE TABLE \"Videos\" ( `course` TEXT NOT NULL, `courseID` TEXT NOT NULL, `week` TEXT NOT NULL, " \
-            "`name` TEXT NOT NULL, `pageUrl` TEXT NOT NULL UNIQUE, `videoUrl` TEXT, `srtUrl` TEXT, `file_exists` " \
-            "BOOLEAN DEFAULT FALSE, PRIMARY KEY(`pageUrl`) )"
+            "`name` TEXT NOT NULL, `pageUrl` TEXT NOT NULL UNIQUE, `file_exists` BOOLEAN DEFAULT FALSE, " \
+            "PRIMARY KEY(`pageUrl`) )"
 
 database = sqlite3.connect("main.db")
 database.execute(statement)
