63710cf2f8f147df2ad07dc0371707fa08436ace
Dylan Barker <dthomasbarker@gmail.com>
2020-12-12 10:15:13 +0000

    Adding appropriate dashes

--- 0_login.py
+++ 0_login.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 
 options = webdriver.ChromeOptions()
-options.add_argument("user-data-dir=selenium/chrome_driver")
+options.add_argument("--user-data-dir=selenium/chrome_driver")
 driver = webdriver.Chrome(executable_path="selenium/chromedriver", options=options)  # for Windows add .exe
 
 driver.get("https://keats.kcl.ac.uk/")
