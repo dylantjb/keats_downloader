978ce2ace5a26e66f5ce6ff5b2486191a5726885
Dylan Barker <dthomasbarker@gmail.com>
2020-12-12 19:26:48 +0000

    Added for developing purposes / files are modified
    Could be used if files were add to, deleted, or moved

---
+++ check_exists.py
@@ -0,0 +1,24 @@
+import os
+import sqlite3
+import subprocess
+
+MAX_NAME_LENGTH = 40
+base_folder = "/home/dylan/OneDrive/Lectures"  # change if necessary
+
+database = sqlite3.connect('main.db')
+
+for video in database.execute("SELECT * FROM Videos WHERE file_exists = FALSE"):
+    dirs = []
+    for i in range(4):
+        dirs.append((video[i][0:MAX_NAME_LENGTH]).strip())
+
+    directory = "{}/{}/{}".format(base_folder, dirs[0], dirs[2])
+    path = "{}/{}.mp4".format(directory, dirs[3])
+    check_subs = 'ffmpeg -i "%s" -c copy -map 0:s -f null - -v 0 -hide_banner && echo true || echo false'
+
+    if os.path.isfile(path) and subprocess.check_output(check_subs % path, shell=True, text=True).strip() == "true":
+        database.execute("UPDATE Videos SET file_exists = TRUE WHERE pageUrl = ?", [video[4]])
+    else:
+        database.execute("UPDATE Videos SET file_exists = FALSE WHERE pageURL = ?", [video[4]])
+
+database.commit()
