67fd0fdb4e6cdedf5844f9a708c2d21fd4f86290
Dylan Barker <dthomasbarker@gmail.com>
2020-12-15 13:07:50 +0000

    Remove database commit
    Closes #2

--- 2_save.py
+++ 2_save.py
@@ -48,13 +48,7 @@ def save(video_url, srt_url, page_url):
         except Exception:  # Connection lost
             sleep(10)
             return True
-
-        try:
-            database.execute("UPDATE Videos SET file_exists = TRUE WHERE pageUrl = ?", [page_url])
-            database.commit()
-        except sqlite3.Error:
-            print("Database is locked ... refer to https://github.com/dylantjb/keats_downloader/issues/2")
-
+        
 
 for video in database.execute("SELECT * FROM Videos WHERE file_exists = FALSE"):
     print(video[1], video[2], video[3])
