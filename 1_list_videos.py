140165bf25a9b89cb26bf6753372ad12345cbe35
Dylan Barker <dthomasbarker@gmail.com>
2020-12-15 11:40:12 +0000

    Add indexing to videos

--- 1_list_videos.py
+++ 1_list_videos.py
@@ -1,3 +1,4 @@
+import os
 import sqlite3
 
 from selenium.webdriver.common.by import By
@@ -15,20 +16,28 @@ options = webdriver.ChromeOptions()
 options.headless = True
 options.add_argument("--user-data-dir=" + os.getcwd() + "/selenium/chrome_driver")
 options.add_argument("--disable-web-security")
-driver = webdriver.Chrome(executable_path="selenium/chromedriver", options=options)  # for Windows add .exe
+driver = webdriver.Chrome(executable_path="selenium/chromedriver", options=options)
 
 driver.get("https://keats.kcl.ac.uk/")
 wait_element = ec.presence_of_element_located((By.ID, 'page-footer'))
 WebDriverWait(driver, 10).until(wait_element)
+
 for course in courses:
     driver.get(course)
     print(course)
     WebDriverWait(driver, 10).until(wait_element)
     videoDicts = driver.execute_script(open("list_videos.js").read())
     videos = []
-    for video in videoDicts:
-        # print(video)
-        videos.append((video['course'], video['courseID'], video['week'], video['name'], video['pageUrl']))
+
+    index = 1
+    for i in range(len(videoDicts) - 1):
+        videoDicts[i]['name'] = str(index) + "_" + videoDicts[i]['name']
+
+        index = index + 1 if videoDicts[i]['week'] == videoDicts[i+1]['week'] else 1
+
+        videos.append((videoDicts[i]['course'], videoDicts[i]['courseID'], videoDicts[i]['week'], videoDicts[i]['name'],
+                       videoDicts[i]['pageUrl']))
+
     database.executemany(
         "INSERT INTO Videos (course, courseID, week, name, pageUrl) VALUES (?, ?, ?, ?, ?) ON CONFLICT(pageUrl) DO UPDATE SET courseID=courseID",
         videos)
